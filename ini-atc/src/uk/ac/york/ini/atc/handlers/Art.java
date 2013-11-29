7c4f5d10c233913c1e7a2827b7680523ce1a8a52
pauliusi <paulius.imbrasas@metasite.net>
2013-11-29 16:58:01 +0000

    #23
    Added Art class which loads all of the textures
    Changed Screens to load from Art
    Now every class can get the screen width and height (stored in Screen)

--- ini-atc/src/uk/ac/york/ini/atc/handlers/Art.java
+++ ini-atc/src/uk/ac/york/ini/atc/handlers/Art.java
@@ -1,5 +1,7 @@
 package uk.ac.york.ini.atc.handlers;
 
+import java.util.Hashtable;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -13,16 +15,72 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  */
 public class Art {
 
-	public static TextureRegion gdx;
+	/**
+	 * A hashtable which stores all of the textures
+	 */
+	private final static Hashtable<String, TextureRegion> textures = new Hashtable<String, TextureRegion>();
 
+	/**
+	 * Initialises loading of texture, should be called once
+	 */
 	public static void load() {
-		gdx = load("data/libgdx.png", 512, 270);
+		Texture combined = loadTexture("data/combinedgraphics.png");
+
+		textures.put("airspace", split(combined, 0, 0, 1280, 720));
+		textures.put("aircraft", split(combined, 1281, 0, 76, 63));
 	}
 
-	public static TextureRegion load(String name, int width, int height) {
+	/**
+	 * Splits a texture from given position with size and width
+	 * 
+	 * @param texture
+	 * @param x
+	 * @param y
+	 * @param size
+	 * @param width
+	 * @return The requested TextureRegion
+	 */
+	private static TextureRegion split(Texture texture, int x, int y, int size,
+			int width) {
+		return new TextureRegion(texture, x, y, size, width);
+	}
+
+	/**
+	 * Wrapper for loading a texture
+	 * 
+	 * @param textureName
+	 * @return Texture
+	 */
+	private static Texture loadTexture(String textureName) {
+		Texture texture = new Texture(Gdx.files.internal(textureName));
+		return texture;
+	}
+
+	/**
+	 * Loads a texture and returns a TextureRegion from taken from 0, 0
+	 * 
+	 * @param name
+	 * @param width
+	 * @param height
+	 * @return
+	 */
+	private static TextureRegion load(String name, int width, int height) {
 		Texture texture = new Texture(Gdx.files.internal(name));
 		TextureRegion region = new TextureRegion(texture, 0, 0, width, height);
 		return region;
+	}
+
+	/**
+	 * Returns a texture region, should be used for all drawing all models
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static TextureRegion getTextureRegion(String key) {
+		if (!textures.containsKey(key))
+			return null;
+
+		return textures.get(key);
 	}
 
 }
--- ini-atc/src/uk/ac/york/ini/atc/screens/TitleScreen.java
+++ ini-atc/src/uk/ac/york/ini/atc/screens/TitleScreen.java
@@ -1,50 +1,28 @@
 package uk.ac.york.ini.atc.screens;
 
+import uk.ac.york.ini.atc.handlers.Art;
 import uk.ac.york.ini.atc.handlers.Input;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class TitleScreen extends Screen {
 
-	private Texture texture;
-	private TextureRegion region;
-
+	private final TextureRegion airspace = Art.getTextureRegion("airspace");
+	private final TextureRegion aircraft = Art.getTextureRegion("aircraft");
 	private int time = 0;
-
-	// Temporary variable to make sure stuff is not loaded twice
-	private boolean loaded = false;
-
-	/**
-	 * A method to load all the required textures for this screen. Should be
-	 * moved to a separate class which preloads everything and puts it into a
-	 * pool, from which everything should be taken.
-	 */
-	public void load() {
-		if (loaded)
-			return;
-
-		loaded = true;
-
-		// loads the texture, whose size must be in power's of two
-		texture = new Texture(Gdx.files.internal("data/nisairspace.png"));
-
-		// select a specific area of the loaded texture, this will be passed
-		// onto a wrapper and drawn
-		region = new TextureRegion(texture, 0, 0, 1280, 720);
-	}
 
 	/**
 	 * The main loop, do all the rendering here
 	 */
 	@Override
 	public void render() {
-		load();
 		spriteBatch.begin();
 
-		draw(region, 0, 0);
+		draw(airspace, 0, 0);
+
+		draw(aircraft, Screen.width / 2, Screen.height / 2);
 
 		drawString("fps: " + Gdx.graphics.getFramesPerSecond(), 10, 20,
 				Color.BLACK);
