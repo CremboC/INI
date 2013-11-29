9e6bf56c9063837bc39b8acc51dd2e1abc2163ba
Philip Drew <philip_drew@hotmail.co.uk>
2013-11-29 18:52:16 +0000

    Created class for aircraft types. Not final

---
+++ ini-atc/src/uk/ac/york/ini/atc/controllers/AircraftType.java
@@ -0,0 +1,15 @@
+package uk.ac.york.ini.atc.controllers;
+
+import com.badlogic.gdx.math.Vector3;
+
+public class AircraftType {
+	public Vector3 coords;
+	public Vector3 velocity;
+	public int radius;
+	public int separationRadius;
+	public String texture;
+	public int maxTurningSpeed;
+	public int maxClimbRate;
+	public int maxSpeed;
+	public boolean isActive;
+}
