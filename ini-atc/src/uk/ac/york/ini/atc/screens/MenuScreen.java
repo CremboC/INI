046fa02c373ed4d696f1fe70c4970bce612c1f89
Paulius <pi514@york.ac.uk>
2013-11-29 17:44:43 +0000

    #24 MenuScreen class stub

---
+++ ini-atc/src/uk/ac/york/ini/atc/screens/MenuScreen.java
@@ -0,0 +1,25 @@
+package uk.ac.york.ini.atc.screens;
+
+import uk.ac.york.ini.atc.handlers.Input;
+
+/**
+ * Menu Screen, displays the menu
+ * 
+ * @author Crembo
+ * 
+ */
+public class MenuScreen extends Screen {
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Input input) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
