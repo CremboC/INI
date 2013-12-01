ce9d5efc14c1d6881562b857f94f0cf81ac27eed
Miguel <mb1069@york.ac.uk>
2013-12-01 15:13:23 +0000

    #24 Linked start game button in MenuScreen, Changed initial screen to menu screen

--- ini-atc/src/uk/ac/york/ini/atc/screens/MenuScreen.java
+++ ini-atc/src/uk/ac/york/ini/atc/screens/MenuScreen.java
@@ -3,7 +3,18 @@ package uk.ac.york.ini.atc.screens;
 import uk.ac.york.ini.atc.handlers.Input;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 /**
  * Menu Screen, displays the menu
@@ -12,16 +23,77 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
  * 
  */
 public class MenuScreen extends Screen {
-
-	private Stage stage;
+	private final Stage stage;
 
 	public MenuScreen() {
-		/**
-		 * Initialise screen on startup -Start and exit buttons
-		 * 
-		 */
+		Skin skin;
+		// TODO Auto-generated constructor stub
 		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
+
+		Table table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+
+		// A skin can be loaded via JSON or defined programmatically, either is
+		// fine. Using a skin is optional but strongly
+		// recommended solely for the convenience of getting a texture, region,
+		// etc as a drawable, tinted drawable, etc.
+		skin = new Skin();
+
+		// Generate a 1x1 white texture and store it in the skin named "white".
+		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin.add("white", new Texture(pixmap));
+
+		skin.add("default", new BitmapFont());
+
+		// Configure a TextButtonStyle and name it "default". Skin resources are
+		// stored by type, so this doesn't overwrite the font.
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
+
+		// Create a button with the "default" TextButtonStyle. A 3rd parameter
+		// can be used to specify a name other than "default".
+		final TextButton startButton = new TextButton("Start game!", skin);
+		table.add(startButton);
+
+		// Add a listener to the button. ChangeListener is fired when the
+		// button's checked state changes, eg when clicked,
+		// Button#setChecked() is called, via a key press, etc. If the
+		// event.cancel() is called, the checked state will be reverted.
+		// ClickListener could have been used, but would only fire when clicked.
+		// Also, canceling a ClickListener event won't
+		// revert the checked state.
+		startButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				System.out.println("Clicked! Is checked: "
+						+ startButton.isChecked());
+				startButton.setText("Good job!");
+				setScreen(new TitleScreen());
+			}
+		});
+
+		table.row();
+
+		final TextButton exitButton = new TextButton("Exit", skin);
+		table.add(exitButton).width(100);
+
+		exitButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Gdx.app.exit();
+			}
+		});
+
+		table.toFront();
 	}
 
 	@Override
@@ -41,6 +113,7 @@ public class MenuScreen extends Screen {
 	 * Called on change of screen, disposes of unneeded resources (ie. prevents
 	 * a memory leak)
 	 */
+	@Override
 	public void removed() {
 		stage.dispose();
 	}
