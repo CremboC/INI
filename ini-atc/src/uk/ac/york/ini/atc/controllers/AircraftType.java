28591b5178853d231f0f87903d1c4eb5ac5a7619
Philip Drew <philip_drew@hotmail.co.uk>
2013-12-01 14:58:13 +0000

    Made setters chainable

--- ini-atc/src/uk/ac/york/ini/atc/controllers/AircraftType.java
+++ ini-atc/src/uk/ac/york/ini/atc/controllers/AircraftType.java
@@ -17,71 +17,80 @@ public class AircraftType {
 		return coords;
 	}
 
-	public void setCoords(Vector3 coords) {
+	public AircraftType setCoords(Vector3 coords) {
 		this.coords = coords;
+		return this;
 	}
 
 	public Vector3 getVelocity() {
 		return velocity;
 	}
 
-	public void setVelocity(Vector3 velocity) {
+	public AircraftType setVelocity(Vector3 velocity) {
 		this.velocity = velocity;
+		return this;
 	}
 
 	public int getRadius() {
 		return radius;
 	}
 
-	public void setRadius(int radius) {
+	public AircraftType setRadius(int radius) {
 		this.radius = radius;
+		return this;
 	}
 
 	public int getSeparationRadius() {
 		return separationRadius;
 	}
 
-	public void setSeparationRadius(int separationRadius) {
+	public AircraftType setSeparationRadius(int separationRadius) {
 		this.separationRadius = separationRadius;
+		return this;
 	}
 
 	public String getTexture() {
 		return texture;
 	}
 
-	public void setTexture(String texture) {
+	public AircraftType setTexture(String texture) {
 		this.texture = texture;
+		return this;
 	}
 
 	public int getMaxTurningSpeed() {
 		return maxTurningSpeed;
 	}
 
-	public void setMaxTurningSpeed(int maxTurningSpeed) {
+	public AircraftType setMaxTurningSpeed(int maxTurningSpeed) {
 		this.maxTurningSpeed = maxTurningSpeed;
+		return this;
 	}
 
 	public int getMaxClimbRate() {
 		return maxClimbRate;
 	}
 
-	public void setMaxClimbRate(int maxClimbRate) {
+	public AircraftType setMaxClimbRate(int maxClimbRate) {
 		this.maxClimbRate = maxClimbRate;
+		return this;
 	}
 
 	public int getMaxSpeed() {
 		return maxSpeed;
 	}
 
-	public void setMaxSpeed(int maxSpeed) {
+	public AircraftType setMaxSpeed(int maxSpeed) {
 		this.maxSpeed = maxSpeed;
+		return this;
 	}
 
 	public boolean isActive() {
 		return isActive;
 	}
 
-	public void setActive(boolean isActive) {
+	public AircraftType setActive(boolean isActive) {
 		this.isActive = isActive;
+		return this;
 	}
 }
