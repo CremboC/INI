fdfacf3dffe71b688105290492aceb2d9f644c15
Paulius <pi514@york.ac.uk>
2013-12-03 16:25:33 +0000

    Changed the title to "Controller Concern"

--- ini-atc-desktop/src/uk/ac/york/ini/atc/Main.java
+++ ini-atc-desktop/src/uk/ac/york/ini/atc/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "INI ATC";
+		cfg.title = "Controller Concern";
 		cfg.useGL20 = false;
 		cfg.width = 1280;
 		cfg.height = 720;
