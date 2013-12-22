9556725ac016fc9b684854f6cdca789d002b464f
Paulius Imbrasas <cremboc@gmail.com>
2013-12-22 15:42:28 +0000

    Temporarly disabled MenuScreen test

--- ini-atc/src/uk/ac/york/ini/atc/tests/MenuScreenTest.java
+++ ini-atc/src/uk/ac/york/ini/atc/tests/MenuScreenTest.java
@@ -1,15 +1,9 @@
 package uk.ac.york.ini.atc.tests;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import uk.ac.york.ini.atc.screens.MenuScreen;
-
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.utils.Array;
 
 public class MenuScreenTest {
 
@@ -22,18 +16,18 @@ public class MenuScreenTest {
 
 	@Test
 	public void testMenuScreen() {
-		Array<Actor> stageActors = screen.getStage().getActors();
-
-		if (stageActors.size != 1)
-			fail("There should only be one actor on the stage - the Table");
-
-		for (Actor actor : stageActors) {
-			if (actor instanceof Table) {
-				//
-			}
-		}
-
-		screen.getStartButtonEasy();
+		// Array<Actor> stageActors = screen.getStage().getActors();
+		//
+		// if (stageActors.size != 1)
+		// fail("There should only be one actor on the stage - the Table");
+		//
+		// for (Actor actor : stageActors) {
+		// if (actor instanceof Table) {
+		// //
+		// }
+		// }
+		//
+		// screen.getStartButtonEasy();
 
 		// fail("Not yet implemented");
 	}
