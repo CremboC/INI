84636177fc6d48f67aa8edd90eb41c8c1e0fa635
Leslie Hor <Leslie.Hor@Gmail.com>
2014-01-14 20:22:46 +0000

    Working JUnit test

--- ini-atc/test/seprini/models/AircraftTest.java
+++ ini-atc/test/seprini/models/AircraftTest.java
@@ -1,6 +1,9 @@
 package seprini.models;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,7 +11,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.badlogic.gdx.math.Vector2;
+
+import seprini.data.Art;
+import seprini.models.types.AircraftType;
+
 public class AircraftTest {
+	
+	private Aircraft aircraft;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,6 +30,23 @@ public class AircraftTest {
 
 	@Before
 	public void setUp() throws Exception {
+		AircraftType defaultAircraft = new AircraftType(); 
+
+
+		defaultAircraft.setCoords(new Vector2(0, 0)).setActive(true)
+		.setMaxClimbRate(0).setMaxSpeed(0.8f).setMaxTurningSpeed(0.4f)
+		.setRadius(15).setSeparationRadius(100)
+		.setTexture(Art.getTextureRegion("aircraft"))
+		.setVelocity(new Vector2(0.8f, 0.8f));
+
+
+		ArrayList<Waypoint> plan = new ArrayList<Waypoint>();
+		plan.add(new Waypoint(3 , 5, true));
+		plan.add(new Waypoint(4 , 7, true));
+
+
+		aircraft = new Aircraft(defaultAircraft, plan, 0);
+
 	}
 
 	@After
@@ -73,12 +100,8 @@ public class AircraftTest {
 
 	@Test
 	public void testGetRadius() {
-		fail("Not yet implemented");
-		/*
-		doul result = Aircraft.getRadius("5.00", result);
-		assertEquals("5.00", result);
-		} 
-		*/
+		
+	assertEquals(15f, aircraft.getRadius(), 100);
 	}
 
 	@Test
