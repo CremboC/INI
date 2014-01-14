8c437415471f5e69d83f0ec26917bd17e1348100
Leslie Hor <Leslie.Hor@Gmail.com>
2014-01-14 14:06:02 +0000

    Creating a test folder and test

---
+++ ini-atc/test/seprini/models/AircraftTest.java
@@ -0,0 +1,122 @@
+package seprini.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AircraftTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdditionalDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAircraft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertWaypoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelay() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncreaseAltitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecreaseAltitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRadius() {
+		//fail("Not yet implemented");
+		doul result = Aircraft.getRadius("5.00", result);
+		assertEquals("5.00", result);
+		}
+
+
+	@Test
+	public void testGetCentreCoords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSeparationRadius() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBreaching() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAltitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsActive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelected() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
