ded55d661b6c55b9d8f6e6496b7392864308f1d6
Miguel <mb1069@york.ac.uk>
2014-01-18 14:00:00 +0000

    #58 Added audio

--- ini-atc/src/seprini/screens/MenuScreen.java
+++ ini-atc/src/seprini/screens/MenuScreen.java
@@ -6,7 +6,7 @@ import seprini.data.Art;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-
+import com.badlogic.gdx.audio.Sound;
 /**
  * Menu Screen, displays the menu with buttons to start and exit the game
  * 
@@ -45,7 +45,8 @@ public class MenuScreen extends Screen {
 
 		// add the table UI
 		root.addActor(ui);
-
+		Sound National = Gdx.audio.newSound(Gdx.files.internal("data/National.mp3"));
+		National.play();
 	}
 
 	@Override
