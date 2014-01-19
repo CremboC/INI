7742bfe68a44bf09b471ba5cc9fb16cfb2629291
Paulius Imbrasas <cremboc@gmail.com>
2014-01-19 20:38:06 +0000

    Texture Filtering updated

--- ini-atc/src/seprini/data/Art.java
+++ ini-atc/src/seprini/data/Art.java
@@ -49,8 +49,7 @@ public class Art {
 	public static void load() {
 		// loads the whole sprite which consists most of the game's textures
 		Texture combined = loadTexture("data/combinedgraphics.png");
-		combined.setFilter(TextureFilter.MipMapLinearLinear,
-				TextureFilter.Linear);
+		combined.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
 		// splits up the sprite into parts and loads them into the table
 		textures.put("airspace", split(combined, 0, 0, 1079, 720));
